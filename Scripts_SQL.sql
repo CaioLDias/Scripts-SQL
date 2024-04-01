cb2faa85c953f855ca0a9a14c06977ad96baaa13
Caio Dias <77282689+CaioLDias@users.noreply.github.com>
2024-04-01 20:30:21 -0300

    Adding scripts SQL file

---
+++ Scripts_SQL.sql
@@ -0,0 +1,21 @@
+-- TABLE
+CREATE TABLE Animal(
+  id INT,
+  nome varchar(60),
+  data_nasc date,
+  peso decimal(8,2),
+  cor varchar(15)
+  );
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE Especie (
+    id_especie INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
